3c17167d13a65ddd56ac7707fd3f257f11817540
Dzonyy <paszcza.jakub@gmail.com>
2020-07-28 07:20:26 +0200

    added new types

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,17 @@
 
+import shuffleArray from './utils'
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionState = Question & { answers: string[] };
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
@@ -8,6 +21,10 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data)
-
+  return data.results.map((questions: Question) => (
+    {
+      ...questions,
+      answer: shuffleArray([]),
+    }
+  ))
 }
